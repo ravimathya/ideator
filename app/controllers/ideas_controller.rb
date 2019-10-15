f276cd8cd65614319b3761c290aad89170e2f650
ravimathya <ravimathya.gmail.com>
2019-10-15 18:36:08 +0545

    last commit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,29 +1,33 @@
 class IdeasController < ApplicationController
     def index
-        @ideas = Idea.all
+        @ideas = Idea.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
     end
 
-    def create
+    def create    
         @idea = Idea.create(idea_params)
+        if @idea.valid?
+            flash[:success] = "Your idea has been posted!"
+        else
+            flash[:alert] = "Woops! Looks like there has been an error!"
+        end
         redirect_to root_path
-    end
-
-    def edit
-        @idea = Idea.find(params[:id])
     end
 
     def update
         @idea = Idea.find(params[:id])
         if @idea.update(idea_params)
-          redirect_to root_path
+            flash[:success] = "The idea has been updated!"
+            redirect_to root_path
         else
-          redirect_to edit_idea_path(params[:id])
+            flash[:alert] = "Woops! Looks like there has been an error!"
+            redirect_to edit_idea_path(params[:id])
         end
     end
 
     def destroy
         @idea = Idea.find(params[:id])
         @idea.destroy
+        flash[:success] = "The idea was successfully deleted!"
         redirect_to root_path
     end
     
